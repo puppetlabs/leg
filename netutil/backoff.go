95a7f4955029e861de72250737f1e94bc1937657
David Shure <david.shure@puppet.com>
2019-01-10 16:49:38 -0800

    Create backoff interface, ExponentialBackoff struct

--- netutil/backoff.go
+++ netutil/backoff.go
@@ -8,12 +8,22 @@ import (
 	"github.com/puppetlabs/insights-stdlib/netutil/errors"
 )
 
-func Backoff(i int) time.Duration {
-	base := 300 * time.Millisecond
-	return time.Duration(math.Exp2(float64(i))) * base
+// Maybe this will be useful at some point?
+type Backoff interface {
+	Backoff(context.Context, int) errors.Error
 }
 
-func Wait(wait time.Duration, ctx context.Context) errors.Error {
+// TODO: Add NextRun method for use with schedulers
+type ExponentialBackoff struct {
+	Multiplier time.Duration
+}
+
+func (b *ExponentialBackoff) Backoff(ctx context.Context, iteration int) errors.Error {
+	timeToWait := time.Duration(math.Exp2(float64(iteration))) * b.Multiplier
+	return Wait(ctx, timeToWait)
+}
+
+func Wait(ctx context.Context, wait time.Duration) errors.Error {
 	select {
 	case <-ctx.Done():
 		// timed out or context was cancelled
