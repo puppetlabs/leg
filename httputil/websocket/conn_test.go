3c35c8714872e79263505ff5c41ebe40833b6dde
Noah Fontes <noah.fontes@puppet.com>
2019-02-03 14:26:12 -0800

    Add simple compatibility check

---
+++ httputil/websocket/conn_test.go
@@ -0,0 +1,14 @@
+package websocket_test
+
+import (
+	"testing"
+
+	gws "github.com/gorilla/websocket"
+	"github.com/puppetlabs/insights-stdlib/httputil/websocket"
+)
+
+func TestConnCompatibility(t *testing.T) {
+	// This test will not compile if Gorilla ever changes the methods we expect
+	// on their struct.
+	var _ websocket.Conn = &gws.Conn{}
+}
