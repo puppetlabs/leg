ba0d4924266ef19f66d3cad1527c266c39f7354a
Brad Heller <brad.heller@gmail.com>
2020-07-31 14:18:44 -0700

    new: Add support for reporting unique IDs for activities

--- instrumentation/activities/activity/activity.go
+++ instrumentation/activities/activity/activity.go
@@ -5,8 +5,15 @@ import "time"
 type ActivityMetadata map[string]string
 
 type Activity struct {
+	// ID is a unique identifier for this activity. Activity data consumers will
+	// use this identifier (when provided) to deduplicated activity data.
+	ID string
+
+	// Name is the name of the activity to report. e.g. "workflow-run"
+	Name string
+
+	// UserID is the identifier of the user that this activity belongs to.
 	UserID    string
-	Name      string
 	Metadata  ActivityMetadata
 	OccuredAt time.Time
 }
