29a8a126ef551b2dc5d88f14b0b001f42c90120d
Noah Fontes <noah.fontes@puppet.com>
2021-06-23 12:36:41 -0700

    Do not mask real errors in retry.Wait when a backoff error occurs

--- timeutil/CHANGELOG.md
+++ timeutil/CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
+### Fixed
+
+* `retry.Wait` no longer masks real errors when a backoff error forces the function to exit early.
+
 ## [0.4.0] - 2021-05-15
 
 ### Added
--- timeutil/pkg/retry/wait_test.go
+++ timeutil/pkg/retry/wait_test.go
@@ -22,9 +22,9 @@ func TestWait(t *testing.T) {
 
 	tests := []struct {
 		Name           string
+		BackoffFactory *backoff.Factory
 		Attempt        func(t *testing.T, ctx context.Context, fn func(context.Context, retry.WorkFunc) error)
 		Step           func(ctx context.Context, cancel context.CancelFunc, c *testclock.FakeClock) func()
-		BackoffFactory *backoff.Factory
 	}{
 		{
 			Name: "Always succeed",
@@ -108,6 +108,34 @@ func TestWait(t *testing.T) {
 				}
 			},
 		},
+		{
+			Name: "Limited backoff without error",
+			BackoffFactory: backoff.Build(
+				backoff.Immediate,
+				backoff.MaxAttempts(3),
+			),
+			Attempt: func(t *testing.T, ctx context.Context, fn func(context.Context, retry.WorkFunc) error) {
+				err := fn(ctx, func(ctx context.Context) (bool, error) {
+					return false, nil
+				})
+				require.EqualError(t, err, "maximum attempts of 3 reached")
+			},
+		},
+		{
+			Name: "Limited backoff with error",
+			BackoffFactory: backoff.Build(
+				backoff.Immediate,
+				backoff.MaxAttempts(3),
+			),
+			Attempt: func(t *testing.T, ctx context.Context, fn func(context.Context, retry.WorkFunc) error) {
+				i := 0
+				err := fn(ctx, func(ctx context.Context) (bool, error) {
+					i++
+					return false, fmt.Errorf("boom %d", i)
+				})
+				require.EqualError(t, err, "boom 3")
+			},
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.Name, func(t *testing.T) {
@@ -115,14 +143,17 @@ func TestWait(t *testing.T) {
 			defer cancel()
 
 			c := testclock.NewFakeClock(time.Now())
-			step := test.Step(ctx, cancel, c)
 
-			opts := []retry.WaitOption{retry.WithClock(
-				clock.NewTimerCallbackClock(
-					k8sext.NewClock(c),
-					func(d time.Duration) { step() },
-				),
-			)}
+			var opts []retry.WaitOption
+			if test.Step != nil {
+				step := test.Step(ctx, cancel, c)
+				opts = append(opts, retry.WithClock(
+					clock.NewTimerCallbackClock(
+						k8sext.NewClock(c),
+						func(d time.Duration) { step() },
+					),
+				))
+			}
 			if test.BackoffFactory != nil {
 				opts = append(opts, retry.WithBackoffFactory(test.BackoffFactory))
 			}
