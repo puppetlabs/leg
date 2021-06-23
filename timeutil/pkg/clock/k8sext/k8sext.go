ab7a6ba969a02591cb6d6bb48d88bf8dc6742ca8
Noah Fontes <noah.fontes@puppet.com>
2021-06-23 12:02:59 -0700

    Work around fake timers with zero duration not firing

--- timeutil/pkg/clock/k8sext/k8sext.go
+++ timeutil/pkg/clock/k8sext/k8sext.go
@@ -11,8 +11,17 @@ import (
 
 type k8sClock struct{ testclock.Clock }
 
-func (kc *k8sClock) NewTimer(d time.Duration) clock.Timer   { return kc.Clock.NewTimer(d) }
-func (kc *k8sClock) NewTicker(d time.Duration) clock.Ticker { return kc.Clock.NewTicker(d) }
+func (kc *k8sClock) NewTimer(d time.Duration) clock.Timer {
+	t := kc.Clock.NewTimer(d)
+	kc.Clock.Sleep(0)
+	return t
+}
+
+func (kc *k8sClock) NewTicker(d time.Duration) clock.Ticker {
+	t := kc.Clock.NewTicker(d)
+	kc.Clock.Sleep(0)
+	return t
+}
 
 func NewClock(delegate testclock.Clock) clock.Clock {
 	return &k8sClock{delegate}
